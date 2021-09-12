2eb7cb44899a5b4e08abe1782c5916860350cb2e
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-09-12 11:24:06 +0700

    Add unit testing for Customer services

---
+++ customer/.customer_test2.go
@@ -0,0 +1,212 @@
+package customer
+
+// NEED RECHECK ON VALIDATION MOCK (RETURN) DATA
+// Check only some key value or whole data?
+
+import (
+	"testing"
+
+	"graphql/mocks"
+	"graphql/customer"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type ServiceCustomer struct {
+	repo customer.Repository
+}
+
+func InitServiceCustomer(repo customer.Repository) customer.CustomerInt {
+	service := new(ServiceCustomer)
+	service.repo = repo
+	return service
+}
+func TestEmailAvailable(t *testing.T) {
+	var testCases = []struct {
+		testName    string
+		email       string
+		expectBool  bool
+		expectError error
+	}{
+		{
+			testName:    "success",
+			email:       "jon@j.j",
+			expectBool:  true,
+			expectError: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		mockGetCustomerByEmailrepo := new(mocks.CustomerInt)
+		mockGetCustomerByEmailrepo.On("IsEmailAvailable", testCase.email).Return(testCase.expectBool, testCase.expectError)
+
+		s := customer.NewCustomerService(mockGetCustomerByEmailrepo)
+
+		status, err := s.IsEmailAvailable(testCase.email)
+
+		assert.Equal(t, testCase.expectBool, status)
+		assert.Equal(t, testCase.expectError, err)
+	}
+}
+
+func TestRegisterCustomer(t *testing.T) {
+	var testCases = []struct {
+		testName string
+		// id int
+		name            string
+		email           string
+		password        string
+		confirmPassword string
+		expectError     error
+	}{
+		{
+			testName: "success",
+			// id: 1,
+			name:            "joni",
+			email:           "jon@j.j",
+			password:        "jojo",
+			confirmPassword: "jojo",
+			expectError:     nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		customerTest := customer.Customer{
+			Name:     testCase.name,
+			Email:    testCase.email,
+			Password: testCase.password,
+		}
+		mockRegisterUserrepo := new(mocks.RegisterUser)
+		mockRegisterUserrepo.On("Register", customerTest).Return(testCase, testCase.expectError)
+
+		s := customer.NewCustomerService(mockRegisterUserrepo)
+
+		customer, err := s.Register(customerTest)
+
+		assert.Equal(t, testCase.name, customer.Name)
+		assert.Equal(t, testCase.expectError, err)
+	}
+
+}
+
+func TestLoginCustomer(t *testing.T) {
+	var testCases = []struct {
+		testName string
+		// id int
+		name        string
+		email       string
+		password    string
+		expectError error
+	}{
+		{
+			testName: "success",
+			// id: 1,
+			name:        "joni",
+			email:       "jon@j.j",
+			password:    "jojo",
+			expectError: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		inputTest := customer.InputLogin{
+			Email:    testCase.email,
+			Password: testCase.password,
+		}
+		mockGetCustomerByEmailrepo := new(mocks.GetCustomerByEmail)
+		mockGetCustomerByEmailrepo.On("LoginCustomer", inputTest).Return(testCase, testCase.expectError)
+
+		s := customer.NewCustomerService(mockGetCustomerByEmailrepo)
+
+		customer, err := s.LoginCustomer(inputTest)
+
+		assert.Equal(t, testCase.email, customer.Email)
+		assert.Equal(t, testCase.expectError, err)
+	}
+}
+
+func TestUpdateCustomerPhone(t *testing.T) {
+	var testCases = []struct {
+		testName string
+		// id int
+		name        string
+		email       string
+		phone       string
+		expectError error
+	}{
+		{
+			testName: "success",
+			// id: 1,
+			name:        "joni",
+			email:       "jon@j.j",
+			phone:       "jojo",
+			expectError: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		email := testCase.email
+		phone := testCase.phone
+		mockGetCustomerByEmailrepo := new(mocks.GetCustomerByEmail)
+		mockGetCustomerByEmailrepo.On("UpdateCustomerPhone", phone, email).Return(testCase.expectError)
+
+		s := customer.NewCustomerService(mockGetCustomerByEmailrepo)
+
+		err := s.UpdateCustomerPhone(email, phone)
+
+		assert.Equal(t, testCase.expectError, err)
+	}
+}
+
+func TestGetCustomerById(t *testing.T) {
+	var testCases = []struct{
+		testName string
+		id int
+		name string
+		email string
+		expectError error
+	}{
+		{
+			testName: "success",
+			id: 1,
+			name: "joni",
+			email: "jon@j.j",
+			expectError: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		id := testCase.id
+		mockGetCustomerById := new(mocks.GetCustomerByID)
+		mockGetCustomerById.On("GetCustomerById", id).Return(testCase, testCase.expectError)
+
+		s := customer.NewCustomerService(mockGetCustomerById)
+
+		customer, err := s.GetCustomerByID(id)
+
+		assert.Equal(t, testCase.id, customer.ID)
+		assert.Equal(t, testCase.expectError, err)
+	}
+}
+
+// func TestChangeProfile(t *testing.T) {
+// 	var testCases = []struct{
+// 		testName string
+// 		id int
+// 		name string
+// 		profileMock string
+// 	}{
+// 		{
+// 			testName: "success",
+// 			id: 1,
+// 			name: "joni",
+// 			profileMock: "aaa",
+// 		},
+// 	}
+	
+// 	for _, testCase := range testCases {
+// 		avatar, _ := ioutil.ReadAll(strings.NewReader(testCase.profileMock))
+
+
+// 	}
+// }
--- customer/customer_test.go
+++ customer/customer_test.go
@@ -1,208 +1,314 @@
-package customer
-
-// NEED RECHECK ON VALIDATION MOCK (RETURN) DATA
-// Check only some key value or whole data?
+package customer_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"graphql/customer"
+	"graphql/handler"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"strings"
 	"testing"
+	"time"
 
-	mocks "graphql/mocks"
-
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
-func InitServiceCustomer(repo Repository) CustomerInt {
-	service := new(ServiceCustomer)
-	service.repo = repo
-	return service
-}
-
-func TestEmailAvailable(t *testing.T) {
-	var testCases = []struct {
-		testName    string
-		email       string
-		expectBool  bool
-		expectError error
-	}{
-		{
-			testName:    "success",
-			email:       "jon@j.j",
-			expectBool:  true,
-			expectError: nil,
-		},
-	}
-
-	for _, testCase := range testCases {
-		mockGetCustomerByEmailrepo := new(mocks.GetCustomerByEmail)
-		mockGetCustomerByEmailrepo.On("IsEmailAvailable", testCase.email).Return(testCase.expectBool, testCase.expectError)
-
-		s := InitServiceCustomer(mockGetCustomerByEmailrepo)
-
-		status, err := s.IsEmailAvailable(testCase.email)
-
-		assert.Equal(t, testCase.expectBool, status)
-		assert.Equal(t, testCase.expectError, err)
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading environment variables")
+		os.Exit(1)
 	}
 }
 
-func TestRegisterCustomer(t *testing.T) {
-	var testCases = []struct {
+func TestMain(m *testing.M) {
+	setup()
+	gin.SetMode(gin.TestMode)
+	os.Exit(m.Run())
+}
+
+func connectDB(dbName string) (*sqlx.DB, error) {
+	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_username"),
+		os.Getenv("DB_password"),
+		os.Getenv("DB_host"),
+		os.Getenv("DB_port"),
+		dbName)
+	db, err := sqlx.Connect("mysql", conn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func setup() {
+	// Create database connection
+	LoadEnv()
+	db, err := connectDB("")
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	dbname := os.Getenv("DB_name")
+
+	// Cleaning database before testing
+	db.Exec("DROP DATABASE IF EXISTS "+dbname)
+	
+	// Create test database
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS "+dbname)
+	if err != nil {
+		log.Printf("Error %s when creating DB\n", err)
+		return
+	}
+	db.Close()
+	
+	// Create database connection
+	db, err = connectDB(dbname)
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	// Create table
+	db.MustExec(`CREATE TABLE customers(
+							id int primary key,
+							name varchar(30),
+							email varchar(30),
+							phone varchar(30),
+							password varchar(30),
+							salt varchar(30),
+							avatar varchar(30),
+							created_at datetime,
+							updated_at datetime)`)
+	// if err != nil {
+	// 	log.Printf("Error %s when creating table", err)
+	// 	return
+	// }
+	
+	// Insert dummy data
+	customer := customer.Customer{}
+	customer.ID = 1
+	customer.Name = "joni"
+	customer.Email = "jon@j.j"
+	customer.Password = "jojo"
+	customer.CreatedAt = time.Now()
+	customer.UpdatedAt = time.Now()
+
+	_, err =db.Exec(`INSERT INTO customers
+				(id, name, email, password, created_at, updated_at)
+				VALUES (?, ?, ?, ?, ?, ?)`,
+				customer.ID, customer.Name, customer.Email,
+				customer.Password, customer.CreatedAt, customer.UpdatedAt)
+	if err != nil {
+		log.Printf("Error %s when inserting dummy data", err)
+	}
+}
+
+func TestCreateCustomer(t *testing.T) {
+	// Create database connection
+	dbname := os.Getenv("DB_name")
+	db, err := connectDB(dbname)
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	// Testcases list
+	testCases := []struct{
 		testName string
-		// id int
-		name            string
-		email           string
-		password        string
-		confirmPassword string
-		expectError     error
-	}{
-		{
-			testName: "success",
-			// id: 1,
-			name:            "joni",
-			email:           "jon@j.j",
-			password:        "jojo",
-			confirmPassword: "jojo",
-			expectError:     nil,
-		},
-	}
-
-	for _, testCase := range testCases {
-		customerTest := Customer{
-			Name:     testCase.name,
-			Email:    testCase.email,
-			Password: testCase.password,
-		}
-		mockRegisterUserrepo := new(mocks.RegisterUser)
-		mockRegisterUserrepo.On("Register", customerTest).Return(testCase, testCase.expectError)
-
-		s := InitServiceCustomer(mockRegisterUserrepo)
-
-		customer, err := s.Register(customerTest)
-
-		assert.Equal(t, testCase.name, customer.Name)
-		assert.Equal(t, testCase.expectError, err)
-	}
-
-}
-
-func TestLoginCustomer(t *testing.T) {
-	var testCases = []struct {
-		testName string
-		// id int
-		name        string
-		email       string
-		password    string
-		expectError error
-	}{
-		{
-			testName: "success",
-			// id: 1,
-			name:        "joni",
-			email:       "jon@j.j",
-			password:    "jojo",
-			expectError: nil,
-		},
-	}
-
-	for _, testCase := range testCases {
-		inputTest := InputLogin{
-			Email:    testCase.email,
-			Password: testCase.password,
-		}
-		mockGetCustomerByEmailrepo := new(mocks.GetCustomerByEmail)
-		mockGetCustomerByEmailrepo.On("LoginCustomer", inputTest).Return(testCase, testCase.expectError)
-
-		s := InitServiceCustomer(mockGetCustomerByEmailrepo)
-
-		customer, err := s.LoginCustomer(inputTest)
-
-		assert.Equal(t, testCase.email, customer.Email)
-		assert.Equal(t, testCase.expectError, err)
-	}
-}
-
-func TestUpdateCustomerPhone(t *testing.T) {
-	var testCases = []struct {
-		testName string
-		// id int
-		name        string
-		email       string
-		phone       string
-		expectError error
-	}{
-		{
-			testName: "success",
-			// id: 1,
-			name:        "joni",
-			email:       "jon@j.j",
-			phone:       "jojo",
-			expectError: nil,
-		},
-	}
-
-	for _, testCase := range testCases {
-		email := testCase.email
-		phone := testCase.phone
-		mockGetCustomerByEmailrepo := new(mocks.GetCustomerByEmail)
-		mockGetCustomerByEmailrepo.On("UpdateCustomerPhone", phone, email).Return(testCase.expectError)
-
-		s := InitServiceCustomer(mockGetCustomerByEmailrepo)
-
-		err := s.UpdateCustomerPhone(email, phone)
-
-		assert.Equal(t, testCase.expectError, err)
-	}
-}
-
-func TestGetCustomerById(t *testing.T) {
-	var testCases = []struct{
-		testName string
-		id int
 		name string
 		email string
-		expectError error
+		password string
+		confirmPassword string
+		expectCode int
+		expectMsg string
 	}{
 		{
 			testName: "success",
-			id: 1,
-			name: "joni",
-			email: "jon@j.j",
-			expectError: nil,
+			name: "joni2",
+			email: "jon@k.k",
+			password: "jiji",
+			confirmPassword: "jiji",
+			expectCode: http.StatusOK,
+			expectMsg: "account successfully created",
+		},
+		{
+			testName: "fail used email",
+			name: "joni3",
+			email: "jon@k.k",
+			password: "jojo",
+			confirmPassword: "jojo",
+			expectCode: http.StatusBadRequest,
+			expectMsg: "email has been used",
+		},
+		{
+			testName: "fail different password",
+			name: "joni4",
+			email: "jon@l.l",
+			password: "jojo",
+			confirmPassword: "jiji",
+			expectCode: http.StatusBadRequest,
+			expectMsg: "password and confirm password is different",
 		},
 	}
 
+	// setting handler
+	r := customer.NewRepo(db)
+	s := customer.NewCustomerService(r)
+	h := handler.NewHandlerCustomer(s, nil)
+
 	for _, testCase := range testCases {
-		id := testCase.id
-		mockGetCustomerById := new(mocks.GetCustomerByID)
-		mockGetCustomerById.On("GetCustomerById", id).Return(Customer, error)
-
-		s := InitServiceCustomer(mockGetCustomerById)
-
-		customer, err := s.GetCustomerByID(id)
-
-		assert.Equal(t, testCase.id, customer.ID)
-		assert.Equal(t, testCase.expectError, err)
+		reqBody, _ := json.Marshal(map[string]string{
+			"name": testCase.name,
+			"email": testCase.email,
+			"password": testCase.password,
+			"confirm_password": testCase.confirmPassword,
+		})
+		g := gin.New()
+		req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(reqBody))
+		res := httptest.NewRecorder()
+		req.Header.Set("Content-Type", "application/json")
+		g.ServeHTTP(res, req)
+		c, _ := gin.CreateTestContext(res)
+		h.CreateCustomer(c)
+		fmt.Println(res)
+		
+		assert.Equal(t, testCase.expectCode, res.Code)
+		assert.True(t, strings.Contains(res.Body.String(), testCase.name))
+		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
 	}
 }
 
-// func TestChangeProfile(t *testing.T) {
-// 	var testCases = []struct{
-// 		testName string
-// 		id int
-// 		name string
-// 		profileMock string
-// 	}{
-// 		{
-// 			testName: "success",
-// 			id: 1,
-// 			name: "joni",
-// 			profileMock: "aaa",
-// 		},
-// 	}
+func TestLogin(t *testing.T) {
+	// Create database connection
+	dbname := os.Getenv("DB_name")
+	db, err := connectDB(dbname)
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	// Testcases list
+	testCases := []struct{
+		testName string
+		email string
+		password string
+		expectCode int
+		expectMsg string
+	}{
+		{
+			testName: "success",
+			email: "jon@k.k",
+			password: "jiji",
+			expectCode: http.StatusUnprocessableEntity,
+			expectMsg: "success login",
+		},
+		{
+			testName: "fail email not found",
+			email: "jin@k.k",
+			password: "jojo",
+			expectCode: http.StatusUnprocessableEntity,
+			expectMsg: "email not found",
+		},
+		{
+			testName: "fail wrong password",
+			email: "jon@k.k",
+			password: "jojo",
+			expectCode: http.StatusUnprocessableEntity,
+			expectMsg: "different password",
+		},
+	}
+
+	// setting handler
+	r := customer.NewRepo(db)
+	s := customer.NewCustomerService(r)
+	h := handler.NewHandlerCustomer(s, nil)
 	
-// 	for _, testCase := range testCases {
-// 		avatar, _ := ioutil.ReadAll(strings.NewReader(testCase.profileMock))
+	for _, testCase := range testCases {
+		reqBody, _ := json.Marshal(map[string]string{
+			"email": testCase.email,
+			"password": testCase.password,
+		})
+		g := gin.New()
+		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(reqBody))
+		res := httptest.NewRecorder()
+		req.Header.Set("Content-Type", "application/json")
+		g.ServeHTTP(res, req)
+		c, _ := gin.CreateTestContext(res)
+		h.Login(c)
+		
+		assert.Equal(t, testCase.expectCode, res.Code)
+		assert.True(t, strings.Contains(res.Body.String(), testCase.email))
+		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
+	}
+}
 
+func TestUpdatePhoneCustomer(t *testing.T) {
+	// Create database connection
+	dbname := os.Getenv("DB_name")
+	db, err := connectDB(dbname)
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
 
-// 	}
-// }
+	// Testcases list
+	testCases := []struct{
+		testName string
+		email string
+		phone string
+		expectCode int
+		expectMsg string
+	}{
+		{
+			testName: "success",
+			email: "jon@k.k",
+			phone: "0812345",
+			expectCode: http.StatusUnprocessableEntity,
+			expectMsg: "successfully udpate",
+		},
+		{
+			testName: "fail email not found",
+			email: "jin@k.k",
+			phone: "0812345",
+			expectCode: http.StatusUnprocessableEntity,
+			expectMsg: "email not found",
+		},
+	}
+
+	// setting handler
+	r := customer.NewRepo(db)
+	s := customer.NewCustomerService(r)
+	h := handler.NewHandlerCustomer(s, nil)
+	
+	for _, testCase := range testCases {
+		currentCustomer := customer.Customer{
+			Email: testCase.email,
+		}
+		reqBody := url.Values{}
+		reqBody.Set("phone", testCase.phone)
+		g := gin.New()
+		req := httptest.NewRequest(http.MethodPut, "/phone", strings.NewReader(reqBody.Encode()))
+		res := httptest.NewRecorder()
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		g.ServeHTTP(res, req)
+		c, _ := gin.CreateTestContext(res)
+		c.Set("currentCustomer", currentCustomer)
+		h.UpdatePhoneCustomer(c)
+		
+		assert.Equal(t, testCase.expectCode, res.Code)
+		assert.True(t, strings.Contains(res.Body.String(), testCase.email))
+		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
+	}
+}
+

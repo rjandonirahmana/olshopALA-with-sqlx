20fe3fdfc5a77aea17df7b2cffcc724f482f78e3
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-09-13 07:37:58 +0700

    Add another code attempt to do unit testing

---
+++ customer/.customer_test3.go
@@ -0,0 +1,449 @@
+package customer_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"graphql/customer"
+	"graphql/handler"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+func LoadEnv() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading environment variables")
+		os.Exit(1)
+	}
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	gin.SetMode(gin.TestMode)
+	os.Exit(m.Run())
+}
+
+func connectDB(dbName string) (*sqlx.DB, error) {
+	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_username"),
+		os.Getenv("DB_password"),
+		os.Getenv("DB_host"),
+		os.Getenv("DB_port"),
+		dbName)
+	db, err := sqlx.Connect("mysql", conn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func setup() {
+	// Create database connection
+	LoadEnv()
+	db, err := connectDB("")
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	dbname := os.Getenv("DB_name")
+
+	// Cleaning database before testing
+	db.Exec("DROP DATABASE IF EXISTS "+dbname)
+	
+	// Create test database
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS "+dbname)
+	if err != nil {
+		log.Printf("Error %s when creating DB\n", err)
+		return
+	}
+	db.Close()
+	
+	// Create database connection
+	db, err = connectDB(dbname)
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	// Create table
+	db.MustExec(`CREATE TABLE customers(
+							id int primary key,
+							name varchar(30),
+							email varchar(30),
+							phone varchar(30),
+							password varchar(30),
+							salt varchar(30),
+							avatar varchar(30),
+							created_at datetime,
+							updated_at datetime)`)
+	// if err != nil {
+	// 	log.Printf("Error %s when creating table", err)
+	// 	return
+	// }
+	
+	// Insert dummy data
+	customer := customer.Customer{}
+	customer.ID = 1
+	customer.Name = "joni"
+	customer.Email = "jon@j.j"
+	customer.Password = "jojo"
+	customer.CreatedAt = time.Now()
+	customer.UpdatedAt = time.Now()
+
+	_, err =db.Exec(`INSERT INTO customers
+				(id, name, email, password, created_at, updated_at)
+				VALUES (?, ?, ?, ?, ?, ?)`,
+				customer.ID, customer.Name, customer.Email,
+				customer.Password, customer.CreatedAt, customer.UpdatedAt)
+	if err != nil {
+		log.Printf("Error %s when inserting dummy data", err)
+	}
+}
+
+func TestCreateCustomer(t *testing.T) {
+	// Create database connection
+	dbname := os.Getenv("DB_name")
+	db, err := connectDB(dbname)
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	// Testcases list
+	testCases := []struct{
+		testName string
+		name string
+		email string
+		password string
+		confirmPassword string
+		expectCode int
+		expectMsg string
+	}{
+		{
+			testName: "success",
+			name: "joni2",
+			email: "jon@k.k",
+			password: "jiji",
+			confirmPassword: "jiji",
+			expectCode: http.StatusOK,
+			expectMsg: "account successfully created",
+		},
+		{
+			testName: "fail used email",
+			name: "joni3",
+			email: "jon@k.k",
+			password: "jojo",
+			confirmPassword: "jojo",
+			expectCode: http.StatusBadRequest,
+			expectMsg: "email has been used",
+		},
+		{
+			testName: "fail different password",
+			name: "joni4",
+			email: "jon@l.l",
+			password: "jojo",
+			confirmPassword: "jiji",
+			expectCode: http.StatusBadRequest,
+			expectMsg: "password and confirm password is different",
+		},
+	}
+
+	// setting handler
+	r := customer.NewRepo(db)
+	s := customer.NewCustomerService(r)
+	h := handler.NewHandlerCustomer(s, nil)
+
+	for _, testCase := range testCases {
+		reqBody, _ := json.Marshal(map[string]string{
+			"name": testCase.name,
+			"email": testCase.email,
+			"password": testCase.password,
+			"confirm_password": testCase.confirmPassword,
+		})
+		g := gin.New()
+		req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(reqBody))
+		res := httptest.NewRecorder()
+		req.Header.Set("Content-Type", "application/json")
+		g.ServeHTTP(res, req)
+		c, _ := gin.CreateTestContext(res)
+		h.CreateCustomer(c)
+		// fmt.Println(res)
+		
+		assert.Equal(t, testCase.expectCode, res.Code)
+		assert.True(t, strings.Contains(res.Body.String(), testCase.name))
+		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
+	}
+}
+
+func TestCreateCustomer2(t *testing.T) {
+	// Create database connection
+	dbname := os.Getenv("DB_name")
+	db, err := connectDB(dbname)
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	// Testcases list
+	testCases := []struct{
+		testName string
+		name string
+		email string
+		password string
+		confirmPassword string
+		expectCode int
+		expectMsg string
+	}{
+		{
+			testName: "success",
+			name: "joni2",
+			email: "jon@k.k",
+			password: "jiji",
+			confirmPassword: "jiji",
+			expectCode: http.StatusOK,
+			expectMsg: "account successfully created",
+		},
+		{
+			testName: "fail used email",
+			name: "joni3",
+			email: "jon@k.k",
+			password: "jojo",
+			confirmPassword: "jojo",
+			expectCode: http.StatusBadRequest,
+			expectMsg: "email has been used",
+		},
+		{
+			testName: "fail different password",
+			name: "joni4",
+			email: "jon@l.l",
+			password: "jojo",
+			confirmPassword: "jiji",
+			expectCode: http.StatusBadRequest,
+			expectMsg: "password and confirm password is different",
+		},
+	}
+
+	// setting handler
+	r := customer.NewRepo(db)
+	s := customer.NewCustomerService(r)
+	h := handler.NewHandlerCustomer(s, nil)
+
+	for _, testCase := range testCases {
+		reqBody, _ := json.Marshal(map[string]string{
+			"name": testCase.name,
+			"email": testCase.email,
+			"password": testCase.password,
+			"confirm_password": testCase.confirmPassword,
+		})
+		// g := gin.Default()
+		req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		res := httptest.NewRecorder()
+		// g.ServeHTTP(res, req)
+		c, _ := gin.CreateTestContext(res)
+		c.Request = req
+		h.CreateCustomer(c)
+		// fmt.Println(res)
+		
+		assert.Equal(t, testCase.expectCode, res.Code)
+		assert.True(t, strings.Contains(res.Body.String(), testCase.name))
+		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
+	}
+}
+
+func TestLogin(t *testing.T) {
+	// Create database connection
+	dbname := os.Getenv("DB_name")
+	db, err := connectDB(dbname)
+	if err != nil {
+		log.Printf("Error %s when opening DB", err)
+		return
+	}
+
+	// Testcases list
+	testCases := []struct{
+		testName string
+		email string
+		password string
+		expectCode int
+		expectMsg string
+	}{
+		{
+			testName: "success",
+			email: "jon@k.k",
+			password: "jiji",
+			expectCode: http.StatusUnprocessableEntity,
+			expectMsg: "success login",
+		},
+		{
+			testName: "fail email not found",
+			email: "jin@k.k",
+			password: "jojo",
+			expectCode: http.StatusUnprocessableEntity,
+			expectMsg: "email not found",
+		},
+		{
+			testName: "fail wrong password",
+			email: "jon@k.k",
+			password: "jojo",
+			expectCode: http.StatusUnprocessableEntity,
+			expectMsg: "different password",
+		},
+	}
+
+	// setting handler
+	r := customer.NewRepo(db)
+	s := customer.NewCustomerService(r)
+	h := handler.NewHandlerCustomer(s, nil)
+	
+	for _, testCase := range testCases {
+		reqBody, _ := json.Marshal(map[string]string{
+			"email": testCase.email,
+			"password": testCase.password,
+		})
+		g := gin.New()
+		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(reqBody))
+		res := httptest.NewRecorder()
+		req.Header.Set("Content-Type", "application/json")
+		g.ServeHTTP(res, req)
+		c, _ := gin.CreateTestContext(res)
+		h.Login(c)
+		
+		assert.Equal(t, testCase.expectCode, res.Code)
+		assert.True(t, strings.Contains(res.Body.String(), testCase.email))
+		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
+	}
+}
+
+// func TestUpdatePhoneCustomer(t *testing.T) {
+// 	// Create database connection
+// 	dbname := os.Getenv("DB_name")
+// 	db, err := connectDB(dbname)
+// 	if err != nil {
+// 		log.Printf("Error %s when opening DB", err)
+// 		return
+// 	}
+
+// 	// Testcases list
+// 	testCases := []struct{
+// 		testName string
+// 		email string
+// 		phone string
+// 		expectCode int
+// 		expectMsg string
+// 	}{
+// 		{
+// 			testName: "success",
+// 			email: "jon@k.k",
+// 			phone: "0812345",
+// 			expectCode: http.StatusUnprocessableEntity,
+// 			expectMsg: "successfully udpate",
+// 		},
+// 		{
+// 			testName: "fail email not found",
+// 			email: "jin@k.k",
+// 			phone: "0812345",
+// 			expectCode: http.StatusUnprocessableEntity,
+// 			expectMsg: "email not found",
+// 		},
+// 	}
+
+// 	// setting handler
+// 	r := customer.NewRepo(db)
+// 	s := customer.NewCustomerService(r)
+// 	h := handler.NewHandlerCustomer(s, nil)
+	
+// 	for _, testCase := range testCases {
+// 		currentCustomer := customer.Customer{
+// 			Email: testCase.email,
+// 		}
+// 		reqBody := url.Values{}
+// 		reqBody.Set("phone", testCase.phone)
+// 		g := gin.New()
+// 		req := httptest.NewRequest(http.MethodPut, "/phone", strings.NewReader(reqBody.Encode()))
+// 		res := httptest.NewRecorder()
+// 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+// 		g.ServeHTTP(res, req)
+// 		c, _ := gin.CreateTestContext(res)
+// 		c.Set("currentCustomer", currentCustomer)
+// 		h.UpdatePhoneCustomer(c)
+		
+// 		assert.Equal(t, testCase.expectCode, res.Code)
+// 		assert.True(t, strings.Contains(res.Body.String(), testCase.email))
+// 		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
+// 	}
+// }
+
+
+// func TestUpdatePhoneCustomer(t *testing.T) {
+// 	// Create database connection
+// 	dbname := os.Getenv("DB_name")
+// 	db, err := connectDB(dbname)
+// 	if err != nil {
+// 		log.Printf("Error %s when opening DB", err)
+// 		return
+// 	}
+
+// 	// Testcases list
+// 	testCases := []struct{
+// 		testName string
+// 		email string
+// 		phone string
+// 		expectCode int
+// 		expectMsg string
+// 	}{
+// 		{
+// 			testName: "success",
+// 			email: "jon@k.k",
+// 			phone: "0812345",
+// 			expectCode: http.StatusUnprocessableEntity,
+// 			expectMsg: "successfully udpate",
+// 		},
+// 		{
+// 			testName: "fail email not found",
+// 			email: "jin@k.k",
+// 			phone: "0812345",
+// 			expectCode: http.StatusUnprocessableEntity,
+// 			expectMsg: "email not found",
+// 		},
+// 	}
+
+// 	// setting handler
+// 	r := customer.NewRepo(db)
+// 	s := customer.NewCustomerService(r)
+// 	h := handler.NewHandlerCustomer(s, nil)
+
+// 	// FOR INTEGRATION TESTING
+// 	for _, testCase := range testCases {
+// 		// currentCustomer := customer.Customer{
+// 		// 	Email: testCase.email,
+// 		// }
+// 		ts := httptest.NewServer()
+// 		reqBody := url.Values{}
+// 		reqBody.Set("phone", testCase.phone)
+// 		res, err := http.PostForm("http://127.0.0.1:8000/phone", reqBody)
+// 		gin.H
+// 		require.Nil(t, err)
+// 		data, err := ioutil.ReadAll(res.Body)
+// 		code := res.StatusCode
+// 		assert.Nil(t, err)
+// 		res.Body.Close()
+
+// 		assert.Equal(t, testCase.expectCode, code)
+// 		assert.True(t, strings.Contains(string(data), testCase.expectMsg))
+// 	}
+// }
+
--- customer/customer_test.go
+++ customer/customer_test.go
@@ -6,14 +6,13 @@ import (
 	"fmt"
 	"graphql/customer"
 	"graphql/handler"
+	"graphql/product"
 	"log"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"os"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -23,26 +22,21 @@ import (
 )
 
 func LoadEnv() {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatalf("Error loading environment variables")
 		os.Exit(1)
 	}
 }
 
-func TestMain(m *testing.M) {
-	setup()
-	gin.SetMode(gin.TestMode)
-	os.Exit(m.Run())
-}
-
-func connectDB(dbName string) (*sqlx.DB, error) {
-	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+func connectDB() (*sqlx.DB, error) {
+	LoadEnv()
+	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True",
 		os.Getenv("DB_username"),
 		os.Getenv("DB_password"),
 		os.Getenv("DB_host"),
 		os.Getenv("DB_port"),
-		dbName)
+		os.Getenv("DB_name"))
 	db, err := sqlx.Connect("mysql", conn)
 	if err != nil {
 		return nil, err
@@ -50,139 +44,87 @@ func connectDB(dbName string) (*sqlx.DB, error) {
 	return db, nil
 }
 
-func setup() {
-	// Create database connection
-	LoadEnv()
-	db, err := connectDB("")
+func setupRouter() *gin.Engine {
+	db, err := connectDB()
 	if err != nil {
-		log.Printf("Error %s when opening DB", err)
-		return
+		log.Fatalf("Error %s when opening DB", err)
+		return nil
 	}
 
-	dbname := os.Getenv("DB_name")
+	customerdb := customer.NewRepo(db)
+	productdb := product.NewRepoProduct(db)
+	customerserv := customer.NewCustomerService(customerdb)
+	productServ := product.NewService(productdb)
 
-	// Cleaning database before testing
-	db.Exec("DROP DATABASE IF EXISTS "+dbname)
-	
-	// Create test database
-	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS "+dbname)
-	if err != nil {
-		log.Printf("Error %s when creating DB\n", err)
-		return
-	}
-	db.Close()
-	
-	// Create database connection
-	db, err = connectDB(dbname)
-	if err != nil {
-		log.Printf("Error %s when opening DB", err)
-		return
-	}
+	productHanlder := handler.NewProductHandler(productServ)
+	customerHandler := handler.NewHandlerCustomer(customerserv, nil)
 
-	// Create table
-	db.MustExec(`CREATE TABLE customers(
-							id int primary key,
-							name varchar(30),
-							email varchar(30),
-							phone varchar(30),
-							password varchar(30),
-							salt varchar(30),
-							avatar varchar(30),
-							created_at datetime,
-							updated_at datetime)`)
-	// if err != nil {
-	// 	log.Printf("Error %s when creating table", err)
-	// 	return
-	// }
-	
-	// Insert dummy data
-	customer := customer.Customer{}
-	customer.ID = 1
-	customer.Name = "joni"
-	customer.Email = "jon@j.j"
-	customer.Password = "jojo"
-	customer.CreatedAt = time.Now()
-	customer.UpdatedAt = time.Now()
+	gin.SetMode(gin.TestMode)
+	c := gin.Default()
+	c.GET("productCategory", productHanlder.GetProductByCategory)
+	c.POST("/register", customerHandler.CreateCustomer)
+	c.POST("/login", customerHandler.Login)
+	c.PUT("/phone", customerHandler.UpdatePhoneCustomer)
+	c.PUT("/avatar", customerHandler.UpdateAvatar)
+	c.POST("/addcart", productHanlder.CreateShopCart)
+	c.POST("/addshopcart", productHanlder.InsertToShopCart)
+	c.GET("listshopcart", productHanlder.GetListProductShopCart)
 
-	_, err =db.Exec(`INSERT INTO customers
-				(id, name, email, password, created_at, updated_at)
-				VALUES (?, ?, ?, ?, ?, ?)`,
-				customer.ID, customer.Name, customer.Email,
-				customer.Password, customer.CreatedAt, customer.UpdatedAt)
-	if err != nil {
-		log.Printf("Error %s when inserting dummy data", err)
-	}
+	return c
 }
 
 func TestCreateCustomer(t *testing.T) {
-	// Create database connection
-	dbname := os.Getenv("DB_name")
-	db, err := connectDB(dbname)
-	if err != nil {
-		log.Printf("Error %s when opening DB", err)
-		return
-	}
-
-	// Testcases list
-	testCases := []struct{
-		testName string
-		name string
-		email string
-		password string
+	testCases := []struct {
+		testName        string
+		name            string
+		email           string
+		password        string
 		confirmPassword string
-		expectCode int
-		expectMsg string
+		expectCode      int
+		expectMsg       string
 	}{
 		{
-			testName: "success",
-			name: "joni2",
-			email: "jon@k.k",
-			password: "jiji",
+			testName:        "success",
+			name:            "joni2",
+			email:           "jon@k.k",
+			password:        "jiji",
 			confirmPassword: "jiji",
-			expectCode: http.StatusOK,
-			expectMsg: "account successfully created",
+			expectCode:      http.StatusOK,
+			expectMsg:       "account successfully created",
 		},
 		{
-			testName: "fail used email",
-			name: "joni3",
-			email: "jon@k.k",
-			password: "jojo",
+			testName:        "fail used email",
+			name:            "joni3",
+			email:           "jon@k.k",
+			password:        "jojo",
 			confirmPassword: "jojo",
-			expectCode: http.StatusBadRequest,
-			expectMsg: "email has been used",
+			expectCode:      http.StatusBadRequest,
+			expectMsg:       "email has been used",
 		},
 		{
-			testName: "fail different password",
-			name: "joni4",
-			email: "jon@l.l",
-			password: "jojo",
+			testName:        "fail different password",
+			name:            "joni4",
+			email:           "jon@l.l",
+			password:        "jojo",
 			confirmPassword: "jiji",
-			expectCode: http.StatusBadRequest,
-			expectMsg: "password and confirm password is different",
+			expectCode:      http.StatusBadRequest,
+			expectMsg:       "password and confirm password is different",
 		},
 	}
 
-	// setting handler
-	r := customer.NewRepo(db)
-	s := customer.NewCustomerService(r)
-	h := handler.NewHandlerCustomer(s, nil)
+	r := setupRouter()
 
 	for _, testCase := range testCases {
 		reqBody, _ := json.Marshal(map[string]string{
-			"name": testCase.name,
-			"email": testCase.email,
-			"password": testCase.password,
+			"name":             testCase.name,
+			"email":            testCase.email,
+			"password":         testCase.password,
 			"confirm_password": testCase.confirmPassword,
 		})
-		g := gin.New()
 		req := httptest.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(reqBody))
 		res := httptest.NewRecorder()
-		req.Header.Set("Content-Type", "application/json")
-		g.ServeHTTP(res, req)
-		c, _ := gin.CreateTestContext(res)
-		h.CreateCustomer(c)
-		fmt.Println(res)
-		
+		r.ServeHTTP(res, req)
+		fmt.Println(testCase.testName, res.Body.String())
 		assert.Equal(t, testCase.expectCode, res.Code)
 		assert.True(t, strings.Contains(res.Body.String(), testCase.name))
 		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
@@ -190,125 +132,107 @@ func TestCreateCustomer(t *testing.T) {
 }
 
 func TestLogin(t *testing.T) {
-	// Create database connection
-	dbname := os.Getenv("DB_name")
-	db, err := connectDB(dbname)
-	if err != nil {
-		log.Printf("Error %s when opening DB", err)
-		return
-	}
-
-	// Testcases list
-	testCases := []struct{
-		testName string
-		email string
-		password string
+	testCases := []struct {
+		testName   string
+		email      string
+		password   string
 		expectCode int
-		expectMsg string
+		expectMsg  string
 	}{
 		{
-			testName: "success",
-			email: "jon@k.k",
-			password: "jiji",
+			testName:   "success",
+			email:      "jon@k.k",
+			password:   "jiji",
 			expectCode: http.StatusUnprocessableEntity,
-			expectMsg: "success login",
+			expectMsg:  "success login",
 		},
 		{
-			testName: "fail email not found",
-			email: "jin@k.k",
-			password: "jojo",
+			testName:   "fail email not found",
+			email:      "jin@k.k",
+			password:   "jojo",
 			expectCode: http.StatusUnprocessableEntity,
-			expectMsg: "email not found",
+			expectMsg:  "email not found",
 		},
 		{
-			testName: "fail wrong password",
-			email: "jon@k.k",
-			password: "jojo",
+			testName:   "fail wrong password",
+			email:      "jon@k.k",
+			password:   "jojo",
 			expectCode: http.StatusUnprocessableEntity,
-			expectMsg: "different password",
+			expectMsg:  "different password",
 		},
 	}
 
-	// setting handler
-	r := customer.NewRepo(db)
-	s := customer.NewCustomerService(r)
-	h := handler.NewHandlerCustomer(s, nil)
-	
+	r := setupRouter()
+
 	for _, testCase := range testCases {
 		reqBody, _ := json.Marshal(map[string]string{
-			"email": testCase.email,
+			"email":    testCase.email,
 			"password": testCase.password,
 		})
-		g := gin.New()
 		req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(reqBody))
 		res := httptest.NewRecorder()
-		req.Header.Set("Content-Type", "application/json")
-		g.ServeHTTP(res, req)
-		c, _ := gin.CreateTestContext(res)
-		h.Login(c)
-		
+		r.ServeHTTP(res, req)
+		fmt.Println(testCase.testName, res.Body.String())
 		assert.Equal(t, testCase.expectCode, res.Code)
 		assert.True(t, strings.Contains(res.Body.String(), testCase.email))
 		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
 	}
 }
 
-func TestUpdatePhoneCustomer(t *testing.T) {
-	// Create database connection
-	dbname := os.Getenv("DB_name")
-	db, err := connectDB(dbname)
-	if err != nil {
-		log.Printf("Error %s when opening DB", err)
-		return
-	}
+// func TestUpdatePhoneCustomer(t *testing.T) {
+// 	// Testcases list
+// 	testCases := []struct {
+// 		testName   string
+// 		email      string
+// 		phone      string
+// 		expectCode int
+// 		expectMsg  string
+// 	}{
+// 		{
+// 			testName:   "success",
+// 			email:      "jon@k.k",
+// 			phone:      "0812345",
+// 			expectCode: http.StatusUnprocessableEntity,
+// 			expectMsg:  "successfully udpate",
+// 		},
+// 		{
+// 			testName:   "fail email not found",
+// 			email:      "jin@k.k",
+// 			phone:      "0812345",
+// 			expectCode: http.StatusUnprocessableEntity,
+// 			expectMsg:  "email not found",
+// 		},
+// 	}
 
-	// Testcases list
-	testCases := []struct{
-		testName string
-		email string
-		phone string
-		expectCode int
-		expectMsg string
-	}{
-		{
-			testName: "success",
-			email: "jon@k.k",
-			phone: "0812345",
-			expectCode: http.StatusUnprocessableEntity,
-			expectMsg: "successfully udpate",
-		},
-		{
-			testName: "fail email not found",
-			email: "jin@k.k",
-			phone: "0812345",
-			expectCode: http.StatusUnprocessableEntity,
-			expectMsg: "email not found",
-		},
-	}
+// 	// r := setupRouter()
+// 	// setting handler
+// 	db, _ := connectDB()
+// 	r := customer.NewRepo(db)
+// 	s := customer.NewCustomerService(r)
+// 	h := handler.NewHandlerCustomer(s, nil)
 
-	// setting handler
-	r := customer.NewRepo(db)
-	s := customer.NewCustomerService(r)
-	h := handler.NewHandlerCustomer(s, nil)
-	
-	for _, testCase := range testCases {
-		currentCustomer := customer.Customer{
-			Email: testCase.email,
-		}
-		reqBody := url.Values{}
-		reqBody.Set("phone", testCase.phone)
-		g := gin.New()
-		req := httptest.NewRequest(http.MethodPut, "/phone", strings.NewReader(reqBody.Encode()))
-		res := httptest.NewRecorder()
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		g.ServeHTTP(res, req)
-		c, _ := gin.CreateTestContext(res)
-		c.Set("currentCustomer", currentCustomer)
-		h.UpdatePhoneCustomer(c)
+// 	for _, testCase := range testCases {
+// 		currentCustomer := customer.Customer{
+// 			Email: testCase.email,
+// 		}
 		
-		assert.Equal(t, testCase.expectCode, res.Code)
-		assert.True(t, strings.Contains(res.Body.String(), testCase.email))
-		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
-	}
-}
+// 		reqBody := url.Values{}
+// 		reqBody.Set("phone", testCase.phone)
 
+// 		res := httptest.NewRecorder()
+		
+// 		req := httptest.NewRequest(http.MethodPut, "/phone", strings.NewReader(reqBody.Encode()))
+// 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		
+// 		c, r := gin.CreateTestContext(res)
+// 		r.ServeHTTP(res, req)
+// 		r.PUT("/phone", h.UpdatePhoneCustomer)
+// 		c.Set("currentCustomer", currentCustomer)
+// 		h.UpdatePhoneCustomer(c)
+
+// 		assert.Equal(t, testCase.expectCode, res.Code)
+// 		assert.True(t, strings.Contains(res.Body.String(), testCase.email))
+// 		assert.True(t, strings.Contains(res.Body.String(), testCase.expectMsg))
+// 	}
+
+// }
